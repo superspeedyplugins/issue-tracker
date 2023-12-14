659d6e7af2c3d101d3c5185b910dfc9d146d08ab
dhilditch <willyhilditch@gmail.com>
2023-12-14 19:41:20 +0200

    Update clone_issue.py

--- scripts/clone_issue.py
+++ scripts/clone_issue.py
@@ -2,6 +2,7 @@ import os
 import json
 import requests
 
+
 def clone_issue(issue_number, token):
     headers = {
         'Authorization': f'token {token}',
@@ -27,69 +28,53 @@ def clone_issue(issue_number, token):
     # Add sourced from text
     body_with_comments += f"Sourced from public issue {issue_url}"
 
-    destination_repo_name = get_first_project_repo(issue_number, headers)
-    destination_repo = f'superspeedyplugins/{destination_repo_name}'
+    ssp_plugins_str = os.getenv('SSP_PLUGINS')
+    ssp_plugins = json.loads(ssp_plugins_str)
 
-    # Prepare data for the new issue
-    new_issue_data = {
-        'title': issue_data['title'],
-        'body': body_with_comments,
-        'labels': issue_data['labels']
-    }
+    issue_labels = issue_data.get('labels', [])
+    destination_repo_slug = get_matching_repo_slug(issue_labels, ssp_plugins)
+    if destination_repo_slug:
+        destination_repo = f'superspeedyplugins/{destination_repo_slug}'
 
-    # Create a new issue in the destination repository
-    create_issue_url = f'https://api.github.com/repos/{destination_repo}/issues'
-    create_response = requests.post(create_issue_url, headers=headers, data=json.dumps(new_issue_data))
+        # Prepare data for the new issue
+        new_issue_data = {
+            'title': issue_data['title'],
+            'body': body_with_comments,
+            'labels': issue_data['labels']
+        }
 
-    if create_response.status_code == 201:
-        print("Issue cloned successfully")
-        new_issue_data = create_response.json()
-        new_issue_url = new_issue_data['html_url']
+        # Create a new issue in the destination repository
+        create_issue_url = f'https://api.github.com/repos/{destination_repo}/issues'
+        create_response = requests.post(create_issue_url, headers=headers, data=json.dumps(new_issue_data))
 
-        # Construct the comment
-        comment_text = f"This issue has been cloned to our private repo: {new_issue_url}"
+        if create_response.status_code == 201:
+            print("Issue cloned successfully")
+            new_issue_data = create_response.json()
+            new_issue_url = new_issue_data['html_url']
 
-        # Post the comment to the original issue
-        comment_url = f'https://api.github.com/repos/{source_repo}/issues/{issue_number}/comments'
-        comment_data = {'body': comment_text}
-        comment_response = requests.post(comment_url, headers=headers, data=json.dumps(comment_data))
+            # Construct the comment
+            comment_text = f"This issue has been cloned to our private repo: {new_issue_url}"
+
+            # Post the comment to the original issue
+            comment_url = f'https://api.github.com/repos/{source_repo}/issues/{issue_number}/comments'
+            comment_data = {'body': comment_text}
+            comment_response = requests.post(comment_url, headers=headers, data=json.dumps(comment_data))
+        else:
+            print("Failed to clone issue")
     else:
-        print("Failed to clone issue")
-    
-def get_first_project_repo(issue_number, headers):
-    # Get the issue timeline
-    source_repo = "superspeedyplugins/issue-tracker"
+        print("No matching repo found")        
 
-    timeline_url = f'https://api.github.com/repos/{source_repo}/issues/{issue_number}/timeline'
-    timeline_response = requests.get(timeline_url, headers=headers)
-    timeline_data = timeline_response.json()
-
-    # Find the first project card event
-    for event in timeline_data:
-        if event['event'] == 'added_to_project':
-            # Get the project card URL from the event
-            project_card_url = event['project_card']['url']
-
-            # Get project card details
-            card_response = requests.get(project_card_url, headers=headers)
-            card_data = card_response.json()
-
-            # Extract the project URL
-            project_url = card_data['project_url']
-
-            # Get project details
-            project_response = requests.get(project_url, headers=headers)
-            project_data = project_response.json()
-
-            # Extract the repository name from the project data
-            repo_name = project_data['owner_url'].split('/')[-1]
-            return repo_name
-
+# Function to find the first matching label and return the corresponding repository slug
+def get_matching_repo_slug(issue_labels, plugins):
+    for label in issue_labels:
+        label_name = label['name']
+        if label_name in plugins:
+            return plugins[label_name]['slug']
     return None
-
+    
 # Environment variables
-issue_number = os.getenv('ISSUE_NUMBER')  # This should be passed from the workflow
-token = os.getenv('GITHUB_TOKEN')  # Your GitHub token
+issue_number = 3  # This should be passed from the workflow
+token = "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"  # Your GitHub token
 
 # Clone the issue
 clone_issue(issue_number, token)
